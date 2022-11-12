3850f5bd9c6caad6a013dc0686f13f6aff95bdb1
Daryl Villalobos <107824734+dariru3@users.noreply.github.com>
2022-11-12 21:38:00 +0900

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,3 +1,48 @@
+--11. join
+SELECT *
+FROM stream s
+INNER JOIN chat c
+  ON s.device_id = c.device_id
+LIMIT 10
+;
+
+--10. 
+SELECT
+  strftime('%H', time) hour,
+  COUNT(*) view_count
+FROM stream
+WHERE country = "US"
+GROUP BY hour
+ORDER BY view_count DESC
+;
+
+--9. review strftime()
+SELECT 
+  time,
+  strftime('%S', time) seconds
+FROM stream
+GROUP BY time
+LIMIT 20;
+
+--8. preview time column
+SELECT time FROM stream LIMIT 10;
+
+--7. genres
+SELECT
+  game,
+  CASE
+    WHEN game in ("League of Legends", "Dota 2", "Heroes of the Storm") THEN "MOBA"
+    WHEN game in ("Counter-Strike: Global Offensive") THEN "FPS"
+    WHEN game in ("DayZ", "ARK: Survival Evolved") THEN "Survival"
+    ELSE "Other"
+  END genre,
+  COUNT(*) count
+FROM stream
+WHERE game IS NOT NULL
+GROUP BY game
+ORDER BY count DESC
+;
+
 --6. view sources
 SELECT
   player,
